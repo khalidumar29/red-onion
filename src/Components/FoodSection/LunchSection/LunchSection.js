ace2c7e2b31759ccd2ef1ad166bc842c4749b398
khalid umar <dreamiwoarld.online@gmail.com>
2022-06-04 22:30:47 +0600

    dinner section do ne

--- src/Components/FoodSection/LunchSection/LunchSection.js
+++ src/Components/FoodSection/LunchSection/LunchSection.js
@@ -1,9 +1,17 @@
 import React from "react";
+import useAllFood from "../../../Hook/useAllFood";
+import FoodCart from "../../../Shared/FoodCart/FoodCart";
 
 const LunchSection = () => {
+  const [foods] = useAllFood();
+  const lunch = foods.filter((food) => food.category === "breakfast");
   return (
     <div>
-      <h1>this is lunch</h1>
+      <div className='bg-white grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5'>
+        {lunch.map((food) => (
+          <FoodCart key={food.id} food={food}></FoodCart>
+        ))}
+      </div>
     </div>
   );
 };
