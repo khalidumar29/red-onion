bd69a257a8947c719c9a5f7e81d5f54de1a81b13
khalid umar <dreamiwoarld.online@gmail.com>
2022-06-04 21:23:33 +0600

    header section done

---
+++ src/Components/FoodSection/FoodSection.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const FoodSection = () => {
+  return (
+    <div>
+      <div class='navbar bg-base-100'>
+        <div class='flex-1'>
+          <a class='btn btn-ghost normal-case text-xl'>daisyUI</a>
+        </div>
+        <div class='flex-none'>
+          <ul class='menu menu-horizontal p-0'>
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FoodSection;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import HeroSection from "../../Components/HeroSection/HeroSection";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <HeroSection></HeroSection>
+    </div>
+  );
 };
 
 export default Home;
